59a01f699db3d89fe6fa4d6fb00f2adcde326c24
Drew Erny <drew@nine.is>
2013-11-11 20:39:58 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CabooseTutorial::Application.config.secret_token = 'b87a80313e4374cae043b223647b5777c45a02799b8cfd7105980f93a6f2e5850bc8a6807d2b9f815af878d65673471ebfb46ca48c92028dc06ebe29c0881d48'
